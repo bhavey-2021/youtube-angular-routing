fc3dcba7f8cae5873b76d072b4a97ed763232f98
bhavey malhotra <bhavey.m@gmail.com>
2021-10-28 20:09:37 +0530

    routing_01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatabindComponent } from './databind/databind.component';
@@ -10,6 +13,18 @@ import { TwoWayComponent } from './databind/two-way/two-way.component';
 import { NgifComponent } from './ngif/ngif.component';
 import { NgswitchComponent } from './ngswitch/ngswitch.component';
 import { NgforComponent } from './ngfor/ngfor.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+
+//whenever we make routes we need make a constant
+const appRoutes:Routes=[
+  {path:'' , component : HomeComponent}, //this will make the home component defaulty open
+  {path:'about' , component : AboutComponent},
+  {path:'contact' , component : ContactComponent},
+  {path:'products' , component : ProductsComponent}
+]//each route will be as a java script object
 
 @NgModule({
   declarations: [
@@ -20,9 +35,14 @@ import { NgforComponent } from './ngfor/ngfor.component';
     TwoWayComponent,
     NgifComponent,
     NgswitchComponent,
-    NgforComponent
+    NgforComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes)   ,
     BrowserModule,
     AppRoutingModule,
     FormsModule
