84322e9258cc5bc9151a405f0bed55526a314631
bhavey malhotra <bhavey.m@gmail.com>
2021-10-28 23:14:10 +0530

    Nested and Child Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,10 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LaptopComponent } from './products/laptop/laptop.component';
+import { TvComponent } from './products/television/tv.component';
+import { MobileComponent } from './products/mobile/mobile.component';
+import { WashingMachineComponent } from './products/washing-machine/washing-machine.component';
 
 //whenever we make routes we need make a constant
 const appRoutes:Routes=[
@@ -28,7 +32,14 @@ const appRoutes:Routes=[
   {path:'home' , component : HomeComponent},
   {path:'about' , component : AboutComponent},
   {path:'contact' , component : ContactComponent},
-  {path:'products' , component : ProductsComponent},
+  //below we are doing child routing of products
+  {path:'products'  , children:[
+    {path:'',component : ProductsComponent},
+    {path:'laptop' , component : LaptopComponent},
+    {path:'mobile' , component : MobileComponent},
+    {path:'television' , component : TvComponent},
+    {path:'washing-machine' , component : WashingMachineComponent}
+  ]},
   {path:'**' , component : PageNotFoundComponent}
   // "**" : when no other route matched the website will redirect to '**'
 ]//each route will be as a java script object
@@ -48,7 +59,11 @@ const appRoutes:Routes=[
     HomeComponent,
     ProductsComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LaptopComponent,
+    TvComponent,
+    MobileComponent,
+    WashingMachineComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes)   ,
